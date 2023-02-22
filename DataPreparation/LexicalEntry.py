561a7816a1de3a863ad4d34a2e5f6accc8222d8f
Joachim Baumeister <joachim.baumeister@denkbares.com>
2023-02-22 18:49:51 +0100

    Initial attempt to read and write as json. Needs more data cleaning.

--- DataPreparation/LexicalEntry.py
+++ DataPreparation/LexicalEntry.py
@@ -8,6 +8,22 @@ class LexicalEntry:
     def __str__(self):
         return f"{self.german}: {self.latin} ({self.lexis})"
 
+    def smart_add(self, entry_value):
+        if self.german:
+            if self.latin:
+                self.lexis = entry_value
+            else:
+                self.latin = entry_value
+        else:
+            self.german = entry_value
+
+    def as_dic(self):
+        d = {"german": self.german, "latin": self.latin, "lexis": self.lexis}
+        return d
+
+    def empty(self):
+        return not(self.german and self.latin and self.lexis)
+
 
 if __name__ == "__main__":
     entry = LexicalEntry('Haus', 'domus', 1)
--- DataPreparation/PDFExportDemo.py
+++ DataPreparation/PDFExportDemo.py
@@ -1,14 +1,17 @@
 from PyPDF2 import PdfReader
+import json
+
+from Nezuko.DataPreparation.LexicalEntry import LexicalEntry
 
 
 class PDFVocReader:
     def __init__(self):
         self.entries = []
-        self.current_entry = []
+        self.current_entry = LexicalEntry()
 
     def read_pdf(self, filename, page=None):
         self.entries = []
-        self.current_entry = []
+        self.current_entry = LexicalEntry()
         reader = PdfReader(filename)
         if isinstance(page, int):
             page = reader.pages[int(page)]
@@ -22,17 +25,23 @@ class PDFVocReader:
         y = tm[5]
         if 50 < y < 780:
             if text == '\n':
-                if len(self.current_entry) > 2:
+                if not self.current_entry.empty():
                     self.entries.append(self.current_entry)
-                    self.current_entry = []
+                    self.current_entry = LexicalEntry()
             else:
-                self.current_entry.append(text.strip())
+                self.current_entry.smart_add(text.strip())
 
 
 if __name__ == "__main__":
-    fname = '/Users/joba/Downloads/Campus_C__Wortschatz_I-III.pdf'
+    wspace = './resources/'
+    fname = wspace + 'Campus_C__Wortschatz_I-III.pdf'
+    outjson = wspace + 'Campus_C__Wortschatz_I-III.json'
     voc_reader = PDFVocReader()
-    dic = voc_reader.read_pdf(fname, page=0)
-    print(dic)
-
+    dic = voc_reader.read_pdf(fname, page=None)
+    result = []
+    for e in dic:
+        result.append(e.as_dic())
+    with open(outjson, 'w') as f:
+        json.dump(result, f, indent=4, ensure_ascii=False)
+    print('Idle.')
 
